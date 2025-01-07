5d6f7aa5efceadcdb7d9476d4aade53764253a9e
leverglowh <leverglowh@gmail.com>
2025-01-07 15:51:48 +0100

    feat: Add new algo handler

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,11 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as chineseSfw from './chinese-sfw'
+import * as chineseText from './chinese-text'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [chineseSfw.shortname]: chineseSfw.handler,
+  [chineseText.shortname]: chineseText.handler,
 }
 
 export default algos
